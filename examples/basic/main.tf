3e3c979ce0fc0c176fb87de4abdc586875770472
Steven B <genseb7@gmail.com>
2020-05-13 12:46:45 -0400

    Hotfix/fix example (#3)
    * update example
    
    * make example runnable

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,11 +2,17 @@ provider "azurerm" {
   version = "~> 1.43.0"
 }
 
+resource "random_pet" "unique_name" {}
+
+locals {
+  unique_storage_account_name = random_pet.unique_name.id
+}
+
 module "azure_file_share" {
   source = "../.."
-  name   = "example"
+  name   = local.unique_storage_account_name
   file_shares = [{
-    name  = "eeoc_web_files"
+    name  = "my_example_fileshare"
     quota = "20"
   }]
 }
