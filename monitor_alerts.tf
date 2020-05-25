e4a272a378a0cab184f18eb070d80cade24b0dfb
Steven B <genseb7@gmail.com>
2020-05-25 09:57:57 -0400

    Prototype sending Azure Metrtics based Alerts (#6)
    * quick add montior metric alerts
    
    * namespace is storage account
    
    * rename for more alerts
    
    * add dimension
    
    * id -> name
    
    * scopes is type list(str)
    
    * use  dynamic nested blocks like our other projects
    
    * fix: each -> dimension

---
+++ monitor_alerts.tf
@@ -0,0 +1,30 @@
+
+resource "azurerm_monitor_metric_alert" "storage_account" {
+  for_each            = var.storage_account_monitor_metric_alert_criteria
+  name                = "${var.name}-${upper(each.key)}"
+  resource_group_name = azurerm_resource_group.this.name
+  scopes              = [azurerm_storage_account.this.id]
+  tags                = var.tags
+
+  action {
+    action_group_id = var.storage_account_monitor_action_group_id
+  }
+
+  # see https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+  criteria {
+    aggregation      = each.value.aggregation
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    metric_name      = each.value.metric_name
+    operator         = each.value.operator
+    threshold        = each.value.threshold
+
+    dynamic "dimension" {
+      for_each = each.value.dimension
+      content {
+        name     = dimension.value.name
+        operator = dimension.value.operator
+        values   = dimension.value.values
+      }
+    }
+  }
+}
